05ed852168d33d605f10d0546bfaab997ea1bc2e
Daniel <dannie005@gmail.com>
2026-09-20 16:50:12 +0100

    feat: update articles configuration and improve routing syntax

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
